acc3172283a72334a159574342ce5701b55025cd
Letinemeso <sh0n556@bk.ru>
2024-03-26 13:59:21 +0300

    animation refactoring
    115

--- include/Misc_Draw_Modules/Animation/Graphics_Component_Reconstructors/Animation_Graphics_Component_Reconstructor__Texture.h
+++ include/Misc_Draw_Modules/Animation/Graphics_Component_Reconstructors/Animation_Graphics_Component_Reconstructor__Texture.h
@@ -3,7 +3,10 @@
 #include <vec2.hpp>
 #include <vec3.hpp>
 
+#include <Stuff/Timer.h>
+
 #include <Components/Graphics_Component_Reconstructor.h>
+#include <Components/Graphics_Component__Texture.h>
 
 
 namespace LMD
@@ -14,8 +17,39 @@ namespace LMD
     public:
         INIT_VARIABLE(LMD::Animation_Graphics_Component_Reconstructor__Texture, LR::Graphics_Component_Reconstructor)
 
+   private:
+       unsigned int m_frames_count = 0;
+       float m_fps = 0;
+       float m_time_before_next_frame = 0.0f;
+       float m_frame_offset_ratio = 0.0f;
+
+       unsigned int m_current_frame = 0;
+       unsigned int m_requested_frame = 0;
+
+   private:
+       bool m_is_paused = true;
+       LST::Timer m_frame_update_timer;
+       unsigned int m_times_to_repeat = 0;
+       unsigned int m_repetitions = 0;
+
+    public:
+        void set_animation_data(unsigned int _frames_count);
+        void set_fps(float _fps);
+        void set_frame(unsigned int _frame);
+
+        void start(unsigned int _cycles = 0, unsigned int _with_frame = 0);
+        void pause();
+        void unpause();
+
+    public:
+        inline unsigned int frames_count() const { return m_frames_count; }
+        inline float fps() const { return m_fps; }
+        inline unsigned int current_frame() const { return m_current_frame; }
+        inline unsigned int repetitions() const { return m_repetitions; }
+        inline bool paused() const { return m_is_paused; }
+
     private:
-        unsigned int m_current_frame = 0;
+       void M_recalculate_frame_data(LR::Graphics_Component__Texture& _texture);
 
     public:
         void update(float _dt) override;
@@ -28,9 +62,19 @@ namespace LMD
     public:
         INIT_VARIABLE(LMD::Animation_Graphics_Component_Reconstructor_Stub__Texture, LR::Graphics_Component_Reconstructor_Stub)
 
+        INIT_FIELDS
+        ADD_FIELD(float, frames_per_second)
+        ADD_FIELD(unsigned int, frames_count)
+        ADD_FIELD(unsigned int, times_to_repeat)
+        FIELDS_END
+
+    public:
+        float frames_per_second = 0;
+        unsigned int frames_count = 0;
+        unsigned int times_to_repeat = 0;
+
     protected:
-        LV::Variable_Base* M_construct_product() const override;
-        void M_init_constructed_product(LV::Variable_Base* _product) const override;
+        INIT_BUILDER_STUB(Animation_Graphics_Component_Reconstructor__Texture)
 
     };
 
--- source/Misc_Draw_Modules/Animation/Graphics_Component_Reconstructors/Animation_Graphics_Component_Reconstructor__Texture.cpp
+++ source/Misc_Draw_Modules/Animation/Graphics_Component_Reconstructors/Animation_Graphics_Component_Reconstructor__Texture.cpp
@@ -1,32 +1,108 @@
 #include <Misc_Draw_Modules/Animation/Graphics_Component_Reconstructors/Animation_Graphics_Component_Reconstructor__Texture.h>
-#include <Misc_Draw_Modules/Animation/Draw_Module__Animation.h>
 
 using namespace LMD;
 
 
+void Animation_Graphics_Component_Reconstructor__Texture::set_animation_data(unsigned int _frames_count)
+{
+    m_frames_count = _frames_count;
+    m_frame_offset_ratio = 1.0f / (float)m_frames_count;
+}
+
+void Animation_Graphics_Component_Reconstructor__Texture::set_fps(float _fps)
+{
+    m_fps = _fps;
+    m_time_before_next_frame = 1.0f / m_fps;
+}
+
+void Animation_Graphics_Component_Reconstructor__Texture::set_frame(unsigned int _frame)
+{
+    m_requested_frame = _frame;
+    m_frame_update_timer.start(m_time_before_next_frame);
+}
+
+
+void Animation_Graphics_Component_Reconstructor__Texture::start(unsigned int _cycles, unsigned int _with_frame)
+{
+    pause();
+
+    m_times_to_repeat = _cycles;
+    m_repetitions = 0;
+
+    set_frame(_with_frame);
+    unpause();
+}
+
+void Animation_Graphics_Component_Reconstructor__Texture::pause()
+{
+    m_frame_update_timer.reset();
+    m_is_paused = true;
+}
+
+void Animation_Graphics_Component_Reconstructor__Texture::unpause()
+{
+    if(!m_frame_update_timer.is_active())
+        m_frame_update_timer.start(m_time_before_next_frame);
+    m_is_paused = false;
+}
+
+
+
+void Animation_Graphics_Component_Reconstructor__Texture::M_recalculate_frame_data(LR::Graphics_Component__Texture& _texture)
+{
+    if(m_current_frame == m_requested_frame)
+        return;
+
+    float current_offset = m_current_frame * m_frame_offset_ratio;
+    float needed_offset = m_requested_frame * m_frame_offset_ratio;
+
+    for(unsigned int i=0; i<_texture.buffer().size(); i += 2)
+        _texture.buffer()[i] += needed_offset - current_offset;
+
+    m_current_frame = m_requested_frame;
+}
+
+
+
 void Animation_Graphics_Component_Reconstructor__Texture::update(float _dt)
 {
-    L_ASSERT(LV::cast_variable<LMD::Draw_Module__Animation>(m_draw_module));
     L_ASSERT(LV::cast_variable<LR::Graphics_Component__Texture>(m_graphics_component));
 
-    Draw_Module__Animation* tf = (Draw_Module__Animation*)m_draw_module;
-    LR::Graphics_Component__Texture* component = (LR::Graphics_Component__Texture*)m_graphics_component;
+    M_recalculate_frame_data(*(LR::Graphics_Component__Texture*)m_graphics_component);
 
+    if(m_is_paused)
+        return;
+
+    m_frame_update_timer.update(_dt);
+    if(m_frame_update_timer.is_active())
+        return;
+
+    if(m_requested_frame + 1 >= m_frames_count)
+        ++m_repetitions;
+
+    if(m_repetitions >= m_times_to_repeat && m_times_to_repeat != 0)
+    {
+        pause();
+        return;
+    }
+
+    set_frame((m_requested_frame + 1) % m_frames_count);
+    m_frame_update_timer.start(m_time_before_next_frame);
 }
 
 
 
 
 
-LV::Variable_Base* Animation_Graphics_Component_Reconstructor_Stub__Texture::M_construct_product() const
+BUILDER_STUB_CONSTRUCTION_FUNC(Animation_Graphics_Component_Reconstructor_Stub__Texture) BUILDER_STUB_CONSTRUCTION_FUNC_DEFAULT_IMPL
+
+BUILDER_STUB_INITIALIZATION_FUNC(Animation_Graphics_Component_Reconstructor_Stub__Texture)
 {
-    return new Animation_Graphics_Component_Reconstructor__Texture;
-}
+    BUILDER_STUB_PARENT_INITIALIZATION;
+    BUILDER_STUB_CAST_PRODUCT;
 
-void Animation_Graphics_Component_Reconstructor_Stub__Texture::M_init_constructed_product(LV::Variable_Base *_product) const
-{
-    LR::Graphics_Component_Reconstructor_Stub::M_init_constructed_product(_product);
+    product->set_animation_data(frames_count);
+    product->set_fps(frames_per_second);
 
-    Animation_Graphics_Component_Reconstructor__Texture* product = (Animation_Graphics_Component_Reconstructor__Texture*)_product;
-
+    product->start(times_to_repeat);
 }
