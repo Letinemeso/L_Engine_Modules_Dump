c532206c1278718f80609712a85868d389b1795e
Артём <sh0n556@bk.ru>
2024-08-31 14:55:39 +0300

    moved Follow_Trajectory_Module here

---
+++ include/Misc_Modules/Follow_Trajectory_Module.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <Data_Structures/List.h>
+#include <Stuff/Key_Frame_Animation.h>
+
+#include <Module.h>
+
+
+namespace LMD
+{
+
+    class Follow_Trajectory_Module : public LEti::Module
+    {
+    public:
+        INIT_VARIABLE(LMD::Follow_Trajectory_Module, LEti::Module);
+
+    public:
+        using Trajectory_KFA = LST::Key_Frame_Animation<glm::vec3>;
+
+    private:
+        bool m_should_run_in_loop = true;
+        bool m_active = true;
+        float m_speed = 0.0f;
+        glm::vec3 m_constant_offset;
+        Trajectory_KFA m_trajectory;
+
+    public:
+        Follow_Trajectory_Module();
+        ~Follow_Trajectory_Module();
+
+    public:
+        inline void set_should_run_in_loop(bool _value) { m_should_run_in_loop = _value; }
+        inline bool should_run_in_loop() const { return m_should_run_in_loop; }
+
+        inline void set_active(bool _value) { m_active = _value; }
+        inline bool active() const { return m_active; }
+
+        inline void set_speed(float _value) { m_speed = _value; }
+        inline float speed() const { return m_speed; }
+
+        inline void set_constant_offset(const glm::vec3& _value) { m_constant_offset = _value; }
+        inline const glm::vec3& constant_offset() const { return m_constant_offset; }
+
+        inline bool trajectory_active() const { return m_trajectory.active(); }
+
+    public:
+        void setup_trajectory(const LDS::List<glm::vec3>& _points_list);
+
+    public:
+        void update(float _dt) override;
+
+    };
+
+
+    class Follow_Trajectory_Module_Stub : public LEti::Module_Stub
+    {
+    public:
+        INIT_VARIABLE(LMD::Follow_Trajectory_Module_Stub, LEti::Module_Stub);
+
+        INIT_FIELDS
+        ADD_FIELD(bool, should_run_in_loop)
+        ADD_FIELD(bool, autostart)
+        ADD_FIELD(float, speed)
+        ADD_FIELD(glm::vec3, constant_offset)
+        ADD_FIELD(LDS::List<glm::vec3>, trajectory)
+        FIELDS_END
+
+    public:
+        bool should_run_in_loop = true;
+        bool autostart = true;
+        float speed = 0.0f;
+        glm::vec3 constant_offset;
+        LDS::List<glm::vec3> trajectory;
+
+    public:
+        INIT_BUILDER_STUB(Follow_Trajectory_Module);
+
+    };
+
+}
---
+++ source/Misc_Modules/Follow_Trajectory_Module.cpp
@@ -0,0 +1,91 @@
+#include <Misc_Modules/Follow_Trajectory_Module.h>
+
+#include <Math_Stuff.h>
+
+using namespace LMD;
+
+
+Follow_Trajectory_Module::Follow_Trajectory_Module()
+{
+    m_trajectory.set_on_update_func(
+        [this](const glm::vec3& _point)
+        {
+            L_ASSERT(transformation_data());
+
+            transformation_data()->set_position(_point + m_constant_offset);
+        });
+}
+
+Follow_Trajectory_Module::~Follow_Trajectory_Module()
+{
+
+}
+
+
+
+void Follow_Trajectory_Module::setup_trajectory(const LDS::List<glm::vec3>& _points_list)
+{
+    L_ASSERT(_points_list.size() > 1);
+
+    m_trajectory.clear();
+
+    LDS::List<glm::vec3>::Const_Iterator it = _points_list.begin();
+
+    glm::vec first_point = *it;
+
+    m_trajectory.add_frame(0.0f, first_point);
+
+    float time_offset = 0.0f;
+
+    while(!it.end_reached())
+    {
+        LDS::List<glm::vec3>::Const_Iterator next_it = it;
+        ++next_it;
+        if(next_it.end_reached())
+            break;
+
+        float distance = LEti::Math::vector_length(*next_it - *it);
+        time_offset += distance / m_speed;
+
+        m_trajectory.add_frame(time_offset, *next_it);
+
+        it = next_it;
+    }
+
+    float distance = LEti::Math::vector_length(*it - first_point);
+    time_offset += distance / m_speed;
+
+    m_trajectory.add_frame(time_offset, first_point);
+
+    m_trajectory.set_run_in_loop(m_should_run_in_loop);
+    m_trajectory.start();
+}
+
+
+
+void Follow_Trajectory_Module::update(float _dt)
+{
+    if(!m_active)
+        return;
+
+    m_trajectory.update(_dt);
+}
+
+
+
+
+
+BUILDER_STUB_DEFAULT_CONSTRUCTION_FUNC(Follow_Trajectory_Module_Stub)
+
+BUILDER_STUB_INITIALIZATION_FUNC(Follow_Trajectory_Module_Stub)
+{
+    BUILDER_STUB_PARENT_INITIALIZATION;
+    BUILDER_STUB_CAST_PRODUCT;
+
+    product->set_should_run_in_loop(should_run_in_loop);
+    product->set_active(autostart);
+    product->set_speed(speed);
+    product->set_constant_offset(constant_offset);
+    if(trajectory.size() > 1)
+        product->setup_trajectory(trajectory);
+}
